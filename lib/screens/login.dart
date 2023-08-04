b175d8f81340f21d3b764de31d3a54a30303a86b
safwanjmuhammed <safwanjmuhammed@gmail.com>
2023-08-04 19:50:44 +0530

    initial_commit

---
+++ lib/screens/login.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+import 'components/textfield.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void showErrorMessage(String message) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(message),
+          );
+        });
+  }
+
+  String _errorMessage = "";
+
+  void validateEmail(String val) {
+    if (val.isEmpty) {
+      setState(() {
+        _errorMessage = "Email can not be empty";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: HexColor("#fed8c3"),
+        body: ListView(
+          padding: const EdgeInsets.fromLTRB(0, 400, 0, 0),
+          shrinkWrap: true,
+          reverse: true,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      height: 535,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: HexColor("#ffffff"),
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Log In",
+                              style: GoogleFonts.poppins(
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
+                                color: HexColor("#4f4f4f"),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(15, 0, 0, 20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Email",
+                                    style: GoogleFonts.poppins(
+                                      fontSize: 18,
+                                      color: HexColor("#8d8d8d"),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  MyTextField(
+                                    onChanged: (() {}),
+                                    controller: passwordController,
+                                    hintText: "hello@gmail.com",
+                                    obscureText: false,
+                                    prefixIcon: const Icon(Icons.mail_outline),
+                                  ),
+                                  Padding(
+                                    padding:
+                                        const EdgeInsets.fromLTRB(8, 0, 0, 0),
+                                    child: Text(
+                                      _errorMessage,
+                                      style: GoogleFonts.poppins(
+                                        fontSize: 12,
+                                        color: Colors.red,
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text(
+                                    "Password",
+                                    style: GoogleFonts.poppins(
+                                      fontSize: 18,
+                                      color: HexColor("#8d8d8d"),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  MyTextField(
+                                    controller: passwordController,
+                                    hintText: "**************",
+                                    obscureText: true,
+                                    prefixIcon: const Icon(Icons.lock_outline),
+                                  ),
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                  ElevatedButton(
+                                    child: const Text('Submit'),
+                                    onPressed: () {},
+                                  ),
+                                  const SizedBox(
+                                    height: 12,
+                                  ),
+                                  Padding(
+                                    padding:
+                                        const EdgeInsets.fromLTRB(35, 0, 0, 0),
+                                    child: Row(
+                                      children: [
+                                        Text("Don't have an account?",
+                                            style: GoogleFonts.poppins(
+                                              fontSize: 15,
+                                              color: HexColor("#8d8d8d"),
+                                            )),
+                                        TextButton(
+                                            child: Text(
+                                              "Sign Up",
+                                              style: GoogleFonts.poppins(
+                                                fontSize: 15,
+                                                color: HexColor("#44564a"),
+                                              ),
+                                            ),
+                                            onPressed: () {}),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Transform.translate(
+                      offset: const Offset(0, -253),
+                      child: Image.asset(
+                        'assets/images/plants',
+                        scale: 1.5,
+                        width: double.infinity,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
